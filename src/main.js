89fa89aa9308511225122d0e2637ffaf43fcddc5
Peter Wong <peterwong.brisbane@gmail.com>
2021-06-25 23:54:55 +0800

    primevue components example

--- src/main.js
+++ src/main.js
@@ -2,4 +2,25 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import PrimeVue from 'primevue/config';
+import InputText from 'primevue/inputtext';
+import InputNumber from 'primevue/inputnumber';
+import Button from 'primevue/button';
+import Card from 'primevue/card';
+
+import 'primevue/resources/themes/saga-green/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css';
+
+const app = createApp(App);
+
+app.use(router);
+app.use(PrimeVue, {ripple: true});
+
+app.component('InputText', InputText);
+app.component('InputNumber', InputNumber);
+app.component('Button', Button);
+app.component('Card', Card);
+
+app.mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Card>
+      <template #content>
+        <h5>Addons</h5>
+        <div class="p-grid p-fluid">
+          <div class="p-col-12 p-md-4">
+            <div class="p-inputgroup">
+              <span class="p-inputgroup-addon">
+                <i class="pi pi-user"></i>
+              </span>
+              <InputText placeholder="Username" />
+            </div>
+          </div>
+
+          <div class="p-col-12 p-md-4">
+            <div class="p-inputgroup">
+              <span class="p-inputgroup-addon">$</span>
+              <InputNumber v-model="value2" suffix="%" :min="0" :max="100" />
+              <span class="p-inputgroup-addon">.00</span>
+            </div>
+          </div>
+
+          <div class="p-col-12 p-md-4">
+            <div class="p-inputgroup">
+              <span class="p-inputgroup-addon">W</span>
+              <InputText placeholder="Website" />
+            </div>
+          </div>
+        </div>
+
+        <form @submit.prevent="greet">
+          <InputText type="text" v-model="text" />
+          <Button type="submit" label="Submit" />
+          <h3>{{ message }}</h3>
+        </form>
+      </template>
+    </Card>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  data() {
+    return {
+      message: null,
+      text: null,
+    };
+  },
+  methods: {
+    greet() {
+      this.message = "Hello " + this.text;
+    },
+  },
+};
 </script>
