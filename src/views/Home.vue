8c1119006b0c8d8752dcd2b6b1fca17ef92a7b0d
Peter Wong <peterwong.brisbane@gmail.com>
2021-06-26 12:59:55 +0800

    calculator form inputs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,49 +3,73 @@
     <Card>
       <template #content>
         <h5>FIRE Calculator</h5>
-        <div class="p-grid p-fluid">
-          <div class="p-col-12 p-md-4">
-            <div class="p-field">
-              <label for="capital">Capital</label>
-              <div class="p-inputgroup">
-                <span class="p-inputgroup-addon">$</span>
-
-                <InputNumber v-model="capital" placeholder="e.g. 200000" suffix="%" :min="0" :max="100" />
-              </div>
-
-              <small id="capital-help">Enter your default capitcal.</small>
-            </div>
-          </div>
-          <div class="p-col-12 p-md-4">
-            <div class="p-field">
-              <label for="capital">Capital</label>
-              <div class="p-inputgroup">
-                <span class="p-inputgroup-addon">$</span>
-
-                <InputNumber v-model="capital" placeholder="e.g. 200000" suffix="%" :min="0" :max="100" />
-              </div>
-
-              <small id="capital-help">Enter your default capitcal.</small>
-            </div>
-          </div>
-          <div class="p-col-12 p-md-4">
-            <div class="p-field">
-              <label for="capital">Capital</label>
-              <div class="p-inputgroup">
-                <span class="p-inputgroup-addon">$</span>
-
-                <InputNumber v-model="capital" placeholder="e.g. 200000" suffix="%" :min="0" :max="100" />
-              </div>
-
-              <small id="capital-help">Enter your default capitcal.</small>
-            </div>
-          </div>
-        </div>
 
         <form @submit.prevent="greet">
-          <InputText type="text" v-model="text" />
-          <Button type="submit" label="Submit" />
-          <h3>{{ message }}</h3>
+          <div class="p-grid p-fluid">
+            <div class="p-col-12 p-md-4">
+              <div class="p-field">
+                <label for="capital">Capital</label>
+                <div class="p-inputgroup">
+                  <span class="p-inputgroup-addon">$</span>
+
+                  <InputNumber v-model="capital" id="capital" placeholder="e.g. 200000" suffix="%" :min="0"
+                               :max="100" />
+                </div>
+              </div>
+
+              <div class="p-field">
+                <label for="annual-expense">Annual Expense</label>
+                <div class="p-inputgroup">
+                  <span class="p-inputgroup-addon">$</span>
+
+                  <InputNumber v-model="annualExpense" id="annual-expense" placeholder="e.g. 100000" suffix="%"
+                               :min="0" />
+                </div>
+              </div>
+
+              <div class="p-field">
+                <label for="dividend-yield">Dividend Yield</label>
+                <div class="p-inputgroup">
+
+                  <InputNumber v-model="dividendYield" id="dividend-yield" placeholder="e.g. 4" suffix="%" :min="0"
+                               :max="100" />
+                  <span class="p-inputgroup-addon">%</span>
+                </div>
+              </div>
+
+              <div class="p-field">
+                <label for="dividend-growth-rate">Dividend Growth Rate</label>
+                <div class="p-inputgroup">
+
+                  <InputNumber v-model="dividendGrowthRate" id="dividend-growth-rate" placeholder="e.g. 20" suffix="%"
+                               :min="0" :max="100" />
+                  <span class="p-inputgroup-addon">%</span>
+                </div>
+              </div>
+
+              <div class="p-field">
+                <label for="dividend-tax-rate">Dividend Tax Rate</label>
+                <div class="p-inputgroup">
+
+                  <InputNumber v-model="dividendTaxRate" id="dividend-tax-rate" placeholder="e.g. 30" suffix="%"
+                               :min="0" :max="100" />
+                  <span class="p-inputgroup-addon">%</span>
+                </div>
+              </div>
+
+              <div class="p-field">
+                <label for="inflation-rate">Inflation Rate</label>
+                <div class="p-inputgroup">
+
+                  <InputNumber v-model="inflationRate" id="inflation-rate" placeholder="e.g. 3" suffix="%" :min="0"
+                               :max="100" />
+                  <span class="p-inputgroup-addon">%</span>
+                </div>
+              </div>
+
+              <Button type="submit" label="Calculate" />
+            </div>
+          </div>
         </form>
       </template>
     </Card>
